ed7f5b6de52bd55f829ada8c90c681088fb1647c
Dzmitry Marozau1 <Dzmitry_Marozau1@epam.com>
2018-09-10 16:29:53 +0300

    Sending e-mail with attachments

--- index.php
+++ index.php
@@ -16,5 +16,7 @@
 
 //require 'complex_objects_in_session.php';
 
-require 'email_sending/email_send.php';
+//require 'email_sending/email_send.php';
+//require 'email_sending/email_send_b.php';
+require 'email_sending/email_send_c.php';
 
