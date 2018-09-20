409f080f59f02c2f25029db306cff5cf7ed86ac4
Dzmitry Marozau1 <Dzmitry_Marozau1@epam.com>
2018-09-20 11:58:41 +0300

    index.php updated

--- index.php
+++ index.php
@@ -1,40 +1,25 @@
 <?php
 
-//require 'simplexml/SimpleXml.php';
-
-//require 'domDocumentXml/domDocumentXml.php';
-
-//require 'trait_and_interface_var_redefinition_in_class/redefinition_of_var.php';
-
-//require 'is_nan_is_infinite.php';
-
-//require 'closure/closure.php';
-
-//require 'error_handler.php';
-
-//require 'rawcookies.php';
-
-//require 'complex_objects_in_session.php';
-
-//require 'email_sending/email_send.php';
-
-//require 'email_sending/email_send_b.php';
-
-//require 'email_sending/email_send_c.php';
-
-//require 'php_errors/php_errors.php';
-
-//require 'iterable/iterable.php';
-
-//require 'iterable/generator.php';
-
-//require 'closure/closure_function_arguments.php';
-
-//require 'isset/multipleArguments.php';
-
-//require 'namespace/namespace.php';
-
-//require 'iterable/iterables.php';
+if (false) {
+  require 'simplexml/SimpleXml.php';
+  require 'domDocumentXml/domDocumentXml.php';
+  require 'trait_and_interface_var_redefinition_in_class/redefinition_of_var.php';
+  require 'is_nan_is_infinite.php';
+  require 'closure/closure.php';
+  require 'error_handler.php';
+  require 'rawcookies.php';
+  require 'complex_objects_in_session.php';
+  require 'email_sending/email_send.php';
+  require 'email_sending/email_send_b.php';
+  require 'email_sending/email_send_c.php';
+  require 'php_errors/php_errors.php';
+  require 'iterable/iterable.php';
+  require 'iterable/generator.php';
+  require 'closure/closure_function_arguments.php';
+  require 'isset/multipleArguments.php';
+  require 'namespace/namespace.php';
+  require 'iterable/iterables.php';
+  require 'iterable/iterablesB.php';
+}
 
 require 'iterable/iterablesB.php';
-
