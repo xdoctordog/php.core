842975053db9c2760ea65556f95b89a9b8892c45
Dzmitry Marozau1 <Dzmitry_Marozau1@epam.com>
2018-09-20 17:16:35 +0300

    AddAttribute SimpleXML

--- domDocumentXml/simple_xml_load_string.php
+++ domDocumentXml/simple_xml_load_string.php
@@ -14,18 +14,29 @@ echo '<pre>';
 $xml->addAttribute('Addtional', 'Info');
 $xml->addChild('Child', 'child');
 
-
 var_dump(['$xml' => $xml]);
 
 var_dump(['$xml->children()' => $xml->children()]);
 var_dump(['$xml->count()' => $xml->count()]);
 
 $newElement = new SimpleXMLElement('<email>Hmm</email>');
+$newElement->addAttribute('score', '10');
+
 var_dump(['$newElement' => $newElement]);
 
-var_dump(['$xml->addChild($newElement)' => $xml->addChild($newElement)]);
+var_dump(['$xml->addChild($newElement)' => $xml->addChild('score', '77')]);
 
 var_dump(['$xml->asXML()' => $xml->asXML()]);
+var_dump(['$xml->asXML()' => $xml->asXML('XML.xml')]);
 
 var_dump(['$xml->xpath(\'/note/to\')' => $xml->xpath('/note/to')]);
 var_dump(['$xml->xpath(\'/note/from\')' => $xml->xpath('/note/from')]);
+
+var_dump(['$xml->score' => $xml->score]);
+
+$xml->score->addAttribute('point', '88');
+
+var_dump(['$xml->score' => $xml->score]);
+
+var_dump(['$xml->asXML()' => $xml->asXML('XML.xml')]);
+var_dump(['$xml->asXML()' => $xml->asXML()]);
